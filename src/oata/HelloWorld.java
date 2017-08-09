85ca480aeb101bea2b0206088a43d6e3ed9549a8
xusiqing <xusiqing@zto.cn>
2017-08-09 13:00:56 +0800

    修改打印信息

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -1,1 +1,1 @@
-package oata; public class HelloWorld { public static void main(String[] args) { System.out.println("Hello World"); } }
+package oata; public class HelloWorld { public static void main(String[] args) { System.out.println("Hello Andrew"); } }
